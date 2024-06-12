c0f595482c76e9fb101c87af44b7ec9067c2375f
Pintowww <asp.arthurpinto@gmail.com>
2024-06-12 09:58:33 -0300

    add a funcao Div

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def sub(a: int, b: int) -> int:
 
 def mult(a:int, b:int) -> int:
     return a * b
+
+def div(a:int, b:int) -> int:
+    if(b != 0):
+        return a / b
+    else:
+        return None
+    
